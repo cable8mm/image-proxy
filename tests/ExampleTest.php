8c45b98953c565773189c498c5cafe97c63d8686
Sam Lee <cable8mm@gmail.com>
2024-02-01 11:59:55 +1300

    Fix some bug about composer and readme

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
